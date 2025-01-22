2e16aaa196c6eefd10cdd9dfedf3a8c9ea8293c2
Si manglam <90334583+simanglam@users.noreply.github.com>
2025-01-22 20:23:02 +0800

    Add .gitignore, update README, and create initial slide draft files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OOP_IN_C
+
+演講內容草稿，請參閱 content_draft。範例程式，請參閱 code_snippet。因 slide 是以 manim 所做，可參閱 http://www.simanglam.com/OOP_IN_C/ 來看到草稿，亦可自行編譯。
---
+++ slide_draft/build.py
@@ -0,0 +1,9 @@
+from os import listdir, system
+from pathlib import Path
+
+scenes = ["test"]
+
+for scene in scenes:
+    system(f"manim-slides render ./scene/{scene}.py {scene}")
+    
+system(f"manim-slides convert {" ".join(scenes)} ./out/index.html")
